b369e5ade97d5e0a1b17f65c7384628ca673702b
hai14894 <hohoanghai14894@gmail.com>
2021-07-18 13:54:10 +1000

    add count to my cart

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,15 +1,23 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
+import { useAppContext } from "../../provider/AppProvider/App.provider";
+
 import "./style.css";
 const NavBar = () => {
+  const {
+    state: { cart },
+  } = useAppContext();
+
+  const totalCount = cart.reduce((acc, current) => (acc += current.count), 0);
+
   return (
     <div className="nav-bar">
       <Link to="/">
         <img src={`/media/logo.png`} alt="logo" />
       </Link>
       <span>Links</span>
-      <span>My Cart</span>
+      <span>My Cart({totalCount})</span>
     </div>
   );
 };
