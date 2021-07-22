44d5ff7e8b5eb9be98dc369c86b3a516edbe56fe
hai14894 <hohoanghai14894@gmail.com>
2021-07-22 20:53:26 +1000

    add animation when hover over CategoryProduct

--- src/category/CategoryProduct/styled.js
+++ src/category/CategoryProduct/styled.js
@@ -47,10 +47,10 @@ export const Overlay = styled("div")`
   transition: all 300ms ease-in;
 `;
 const btnCss = `
-  width: 60%;
+  width: 50%;
   height: 36px;
   font-family: Arial;
-  font-size: 13px;
+  font-size: 16px;
   color: white;
   border-style: none;
   cursor: pointer;
@@ -64,8 +64,11 @@ export const ViewDetails = styled(Link)`
   background-color: black;
   text-decoration: none;
   margin-bottom: ${size.sm}px;
+  transition: all 0.3s ease-in-out 0s;
   &:hover {
-    color: white;
+    transform: scale(1.05);
+    background-color: white;
+    color: black;
   }
   @media (min-width: ${breakpoints.md}px) {
     margin-bottom: ${size.md}px;
@@ -74,4 +77,10 @@ export const ViewDetails = styled(Link)`
 export const AddToCart = styled("button")`
   ${btnCss}
   background-color: rgb(74,74,77);
+  transition: all 0.4s ease-in-out 0s;
+  &:hover {
+    transform: scale(1.05);
+    background-color: white;
+    color: black;
+  }
 `;
